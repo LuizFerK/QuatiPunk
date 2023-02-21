20f514bf32b5f1e4eb138afc8856c0cd85a6c552
LuizFerK <luizfernandoklein@live.com>
2023-02-21 01:34:10 -0300

    feat: Enable CORS

--- backend/src/main/java/br/com/quatipunk/controllers/OrdersController.java
+++ backend/src/main/java/br/com/quatipunk/controllers/OrdersController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,6 +32,7 @@ import br.com.quatipunk.repositories.ProductRepository;
  *
  * Manage the app orders
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class OrdersController {
   @Autowired
--- backend/src/main/java/br/com/quatipunk/controllers/ProductsController.java
+++ backend/src/main/java/br/com/quatipunk/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +25,7 @@ import br.com.quatipunk.repositories.ProductRepository;
  *
  * Manage the app products
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ProductsController {
   @Autowired
